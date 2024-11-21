8d221beb0ff8179f0ee879976f950ac43724517c
e7h4n <perfectworks@gmail.com>
2024-11-21 20:05:42 +0800

    remove happy-dom

--- vitest.config.js
+++ vitest.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
-        environment: 'happy-dom',
         coverage: {
             reporter: ['text', 'json-summary', 'json', 'cobertura'],
             provider: 'v8'
