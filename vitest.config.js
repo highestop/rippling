31ebb96a2e1019593780acb64d1816ee925b36ed
e7h4n <perfectworks@gmail.com>
2024-11-29 22:13:54 +0800

    ci: add codspeed

--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vitest/config'
+import codspeedPlugin from "@codspeed/vitest-plugin";
 
 export default defineConfig({
+    plugins: [codspeedPlugin()],
     test: {
         coverage: {
             reporter: ['text', 'json-summary', 'json', 'cobertura'],
