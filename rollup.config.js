a5a69cc368252656dd67c598578c4acdb6383568
e7h4n <perfectworks@gmail.com>
2024-12-05 21:09:34 +0800

    fix: build package

--- rollup.config.js
+++ rollup.config.js
@@ -3,127 +3,81 @@ import * as fs from "node:fs";
 import * as path from "node:path";
 import { babel } from "@rollup/plugin-babel";
 import { dts } from "rollup-plugin-dts";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const projectRootDir = path.resolve(__dirname);
 
 /** @type {import('./package.json')} */
 const pkg = JSON.parse(
-    fs.readFileSync("./package.json", { encoding: "utf-8" })
+    fs.readFileSync("./package.json", { encoding: "utf-8" }),
 );
+
+const targetCJS = pkg.exports["."].require
+const targetES = pkg.exports["."].import
 
 /** @type { import('rollup').RollupOptions } */
 const commonConfig = {
+    input: "./src/index.ts",
+    external: [
+        ...Object.keys(pkg.peerDependencies),
+    ],
     onwarn: (warning) => {
         throw new Error(warning?.message);
     },
-    external: [
-        ...Object.keys(pkg.peerDependencies),
-    ]
+};
+
+/** @type { import('rollup').RollupOptions } */
+const dtsConfig = {
+    plugins: [
+        dts({
+            respectExternal: true,
+            tsconfig: path.resolve(projectRootDir, "./tsconfig.json"),
+        }),
+    ],
+    output: [
+        {
+            file: targetCJS.replace(/\.cjs$/, ".d.cts"),
+        },
+        {
+            file: targetES.replace(/\.js$/, ".d.ts"),
+        },
+    ],
 }
 
-/** @type { Record<string, { input: string, output: { cjs: string, esm: string, dts: { cjs: string, esm: string } } }> } */
-const entries = {
-        index: {
-            input: "./src/index.ts",
-            output: {
-                cjs: "./dist/index.js",
-                esm: "./dist/esm/index.mjs",
-                dts: {
-                    cjs: "./dist/index.d.ts",
-                    esm: "./dist/esm/index.d.mts"
-                }
-            }
+/** @type { import('rollup').RollupOptions } */
+const sourceConfig = {
+    plugins: [
+        nodeResolve({
+            extensions: ['.ts']
+        }),
+        babel({
+            exclude: "node_modules/**",
+            extensions: [".ts"],
+            babelHelpers: "bundled",
+            configFile: path.resolve(projectRootDir, "./babel.config.json"),
+        }),
+    ],
+    output: [
+        {
+            file: targetCJS,
+            format: "cjs",
         },
-        core: {
-            input: "./src/core/index.ts",
-            output: {
-                cjs: "./dist/core.js",
-                esm: "./dist/esm/core.mjs",
-                dts: {
-                    cjs: "./dist/core.d.ts",
-                    esm: "./dist/esm/core.d.mts"
-                }
-            }
+        {
+            file: targetES,
+            format: "es",
         },
-        react: {
-            input: "./src/react/index.ts",
-            output: {
-                cjs: "./dist/react.js",
-                esm: "./dist/esm/react.mjs",
-                dts: {
-                    cjs: "./dist/react.d.ts",
-                    esm: "./dist/esm/react.d.mts"
-                }
-            }
-        },
-        debug: {
-            input: "./src/debug/index.ts",
-            output: {
-                cjs: "./dist/debug.js",
-                esm: "./dist/esm/debug.mjs",
-                dts: {
-                    cjs: "./dist/debug.d.ts",
-                    esm: "./dist/esm/debug.d.mts"
-                }
-            }
-        }
-    };
+    ],
+}
 
-    /** @type { (entry: typeof entries[keyof typeof entries]) => import('rollup').RollupOptions } */
-    const dtsConfig = (entry) => {
-        return {
-            ...commonConfig,
-            input: entry.input,
-            plugins: [
-                dts({
-                    respectExternal: true,
-                    tsconfig: path.resolve(projectRootDir, "./tsconfig.json"),
-                }),
-            ],
-            output: [
-                {
-                    file: entry.output.dts.cjs,
-                },
-                {
-                    file: entry.output.dts.esm,
-                },
-            ],
-        }
-    };
-
-    /** @type { (entry: typeof entries[keyof typeof entries]) => import('rollup').RollupOptions } */
-    const sourceConfig = (entry) => ({
+/** @type { Array<import('rollup').RollupOptions> } */
+export default [
+    {
         ...commonConfig,
-        input: entry.input,
-        plugins: [
-            nodeResolve({
-                extensions: [".ts"],
-            }),
-            babel({
-                exclude: "node_modules/**",
-                extensions: [".ts"],
-                babelHelpers: "bundled",
-                configFile: path.resolve(projectRootDir, "./babel.config.json"),
-            }),
-        ],
-        output: [
-            {
-                file: entry.output.cjs,
-                format: "cjs",
-            },
-            {
-                file: entry.output.esm,
-                format: "es",
-            },
-        ],
-    });
-
-    /** @type { Array<import('rollup').RollupOptions> } */
-    export default [
-        ...Object.values(entries).flatMap((entry) => [
-            sourceConfig(entry),
-            dtsConfig(entry),
-        ]),
-    ];
+        ...sourceConfig,
+    },
+    {
+        ...commonConfig,
+        ...dtsConfig,
+    },
+];
