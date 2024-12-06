377e19b6975dd99c3c2bda09f0f75a69ac2053f1
e7h4n <perfectworks@gmail.com>
2024-12-07 01:48:45 +0800

    test: fix coverage report, exlucde useless files

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, coverageConfigDefaults } from "vitest/config";
 
 export default defineConfig({
   test: {
     coverage: {
       reporter: ["text", "json-summary", "json", "cobertura"],
       provider: "v8",
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        "**/dist/**",
+        "**/types/**",
+        "**/*.config.*",
+      ],
     },
   },
 });
